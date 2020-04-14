b1cd0a4cc9086bb64e2a6928e06f7ce975e0125b
Veljko <veljkos82@gmail.com>
2020-04-14 19:32:14 +0200

    Chart is passing all tests.

--- barchart.js
+++ barchart.js
@@ -1,4 +1,4 @@
-let res = null;
+let result = null;
 $(document).ready(() => {
 
     let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
@@ -6,14 +6,28 @@ $(document).ready(() => {
         url: url, 
         success: (response) => {
 
-            res = JSON.parse(response);
+            result = JSON.parse(response);
 
-            let dataset = [];
+            // Dimensions of chart.
+            const w = 800;
+            const h = 600;
 
-            for(let i=0;i<res.data.length;i++){
+            //Margin to be set between chart and outher container.
+            const m = { top: 40, right: 40, bottom: 40, left: 40 };
 
-                let year = res.data[i][0];
-                let month = res.data[i][0].substring(res.data[i][0].length-5, res.data[i][0].length-3);
+            //How many bars are. Number of element in array are the number of bars.
+            let barsQuantity = result.data.length;
+
+            //Calculating bar width. Width of chart divided by how many bars are. Result is width for individual bar.
+            let barWidth = w / barsQuantity;
+
+            //Getting data.
+            let dataset = [...result.data];
+            
+            //Getting quarter.
+            for(let i=0;i<result.data.length;i++){
+
+                let month = result.data[i][0].substring(result.data[i][0].length-5, result.data[i][0].length-3);
                 let q = null;
 
                 if(parseInt(month) <= 3){
@@ -31,130 +45,129 @@ $(document).ready(() => {
                 if(parseInt(month) <= 12 && parseInt(month)>9 && parseInt(month)>6 && parseInt(month)>3){
                     q = "Q4";
                 }
-                dataset.push([res.data[i][1],year,q]);
+                dataset[i].push(q);
 
             }
-
-            const w = 800;
-            const h = 600;
-
-            //set the margins
-            const m = { top: 40, right: 40, bottom: 40, left: 40 };
-
+            
+            //Calculating smallest and highest value for gdp part of dataset.
+            let minVal = d3.min(dataset, (d) => d[1]);
+            let maxVal = d3.max(dataset, (d) => d[1]);
+            
+            //Drawing chart in element in html document and adding appropriate margin.
             const svg = d3.select("#demo").append("svg")
-                .attr("width", w + m.left + m.right)//Sirina i visina celokupnog svg elementa.
+                .attr("width", w + m.left + m.right)
                 .attr("height", h + m.top + m.bottom);
 
-            //Pozicioniranje samog chart-a unutar svg-a.
+            //Translating axises to accomodate for margin.If we don't, ticks and numbers would look cut of.
             const g = svg.append("g").attr("transform", "translate(" + m.left + "," + m.top + ")");
 
-            // the xScale for the bar chart will be a band scale
-            /*range je duzina linije po x osi koja se gleda od sirine svg-a.
-            domain su vrednosti koje se automatski nalaze kada se indeksi dataset-a
-            uzmu kao parametar. Zatim se indeksi pravilno rasporede po sirini(width-w), tj. range-u.*/
-            let xScale = d3.scaleBand()
-                    .range([0, w])
-                    //Since the domain is about the index of the values, the domain must be an array of the indices for the x-axis scale
-                    .domain(dataset.map( (d,i) => { 
-
-                        return i; 
-
-                    }));
-
-            //for the y-axis scale since it is a linear scale you just mention the array extent as the domain
-            /*Kod y ose je slicno, ali malo drugacije. U domain-u umesto indeksa, 
-            prikazuju se istoimene vrednosti, a range je visina(h) po kome se redjaju vrednosti.*/
+            //Setting y axis.
             let yScale = d3.scaleLinear()
-                    .range([h, 0])
-                    .domain([0, dataset[dataset.length-1][0]]);
+            .range([h, 0])//Actual y axis length.
+            .domain([0, maxVal]);//Setting values from zero to max value on y axis.
+            
+            //Drawing y axis.
+            let yAxis = d3.axisLeft(yScale);
 
-            // draw the axis
-            let xScale1 = d3.scaleBand()
-                    .range([0, w])
-                    //Since the domain is about the index of the values, the domain must be an array of the indices for the x-axis scale
-                    .domain(dataset.map( (d,i) => { 
+            //Getting actual (only)year parts from dataset.
+            let years = dataset.map(year => {
+                return new Date(year[0]);
+            });
 
-                        return d[1].substr(0, 4); 
+            //Calculating year.
+            let minYear = new Date(d3.min(years));//Calculating first year in dataset.
+            let maxYear = new Date(d3.max(years));//Calculating last year in dataset.
+            maxYear.setMonth(maxYear.getMonth() + 3);//Compensating for discrapancy.
 
-                    })); 
+            //Setting x axis.
+            let xScale = d3.scaleTime()
+                    .range([0, w])//Actual x axis length.
+                    .domain([minYear, maxYear]);//Setting values from zero to max year on x axis.
 
-            let ind = null;
-            const xAxis = d3.axisBottom(xScale1).tickFormat((d,i) => {
-                
-                if(parseInt(d) === 1950){
-                    ind = i;
-                    return d;
-                }
-
-                if(ind+5===i){
-                    ind = i;
-                    return d;
-                }
-                
-            }); //X axis
-
+            //Drawing y axis.
+            const xAxis = d3.axisBottom(xScale);
+            
+            //Apending x axis to chart.
             g.append("g")
                 .attr("transform", "translate(0," + h + ")")
                 .attr("class", "tick axis")
                 .attr("id", "x-axis")
                 .call(xAxis);
 
-            const yAxis = d3.axisLeft(yScale); //Y axis
+            //Apending y axis to chart.
             g.append("g")
-                .attr("transform", "translate(0, 0)")//Ako se pravilno setuje u yScale, onda se ovde ne mora raditi translacija.
+                .attr("transform", "translate(0, 0)")
                 .attr("class", "tick axis")
                 .attr("id", "y-axis")
+                .attr("fill", "red")
                 .call(yAxis);
 
-            /*Dakle x osa je width i po njoj se redjaju indeksi,
-            y osa je height i po njoj se redjaju vrednosti od gore pomenutih indeksa.*/
-            /*
-            Kada se sve izracuna  u xAxis i yAxis, onda se prosledjuje 
-            kao parametar za x i y koordinatu bar-a.
-            */
+            //Creating tooltip element.
+            const tooltip = d3.select('#demo')
+                .append('div')
+                .attr('id', 'tooltip')
+                .style('opacity', 0);
 
-            //draw the rectangles
+            //Creating bar/s.
             g.selectAll("rect")
                 .data(dataset)
                 .enter()
                 .append("rect")
-                .attr("id",(d,i) => {
-                    return "tipX"+i
+                .attr("x", (d,i) => {
+                    /*Setting x coordinate by multiplying index of dataset with bar width.
+                     Bars set up'd this way are next to each other, w/o padding or margin.*/
+                    return (i*barWidth);
+                  })
+                .attr("y", (d,i) => { 
+                    /*Setting y coordinate by calculating the difference 
+                    between max height availiable and height of the particular bar.
+                    Height of the particular bar is calculated by dividing value 
+                    for particular bar from dataset with maximum value from dataset.
+                    For last value, we are getting result of 1(one). That means last one is occupaying
+                    whole height. In other, it means, it gets zero spaces avaliable from top.*/
+                    let percent = d[1] / maxVal;
+                    console.log(percent);
+                    return (h - (h * percent));
                 })
-                .attr("x", (d, i) => {//d je vrednost iz niza.
-                    return xScale(i);//koordinate za tik, odnosno sredinu pojedinacnog bar-a.
-                }) //the displacement along the x is dependendant on the index and the xScale
-                .attr("y", (d) => { 
-                    return yScale(d[0]);//Proracunata vrednost displejsmenta od vrha. 
-                }) //the displacement along the y is dependant on the value and the yScale
-                .attr("height", (d) => { 
-                    return h - yScale(d[0]); //Proracun visine pojedinacnog bara.
-                }) //the height is the difference between the displacement down and the height of the chart h
-                .attr("width", xScale.bandwidth()) //Proracun sirine el. bez padinga. //the width of the rectangles is dependant on the bandwidth
+                .attr("height", (d,i) => { 
+                    /*Height is calculated by dividing particular bar value with maximum value.
+                    In this, value calculated presents as percentage of maximum avaliable.
+                    Last value divided by max values results in 1(one). Which implies that that
+                    particular bar occupies whole height. To finally return value needed for height,
+                    percentage is multiplied with height variable which returns 
+                    actual height value for particular bar.*/
+                    let percent = d[1] / maxVal;
+                    return h*percent;
+                })
+                .attr("width", (d,i) => { 
+                    //Setting bar width previosly calculated. It is the same foor each bar.
+                    return barWidth;
+                })
                 .attr("data-date", (d,i) => {
-                    return d[1];
-                })
-                .attr("data-gdp", (d,i) => {
+                    //Setting data-date attribute with value.
                     return d[0];
                 })
-                .attr("class", "bar")
-                .append("title")
-                .attr("data-date", (d,i) => {
+                .attr("data-gdp", (d,i) => {
+                    //Setting data-gdp attribute with value.
                     return d[1];
                 })
-                .attr("id", "tooltip")
-                .text((item, i) => {
-                    
-                    return item[1].substr(0, 4)+" "+item[2]+" \n$"+item[0]+" Billion";
-                    
-                });
-                
+                //Setting tooltip made from absolutely positioned div.
+                .on('mouseover', (d, i) => {
+                    tooltip.style('opacity', 0.5);
+                    tooltip.html("<div style='margin-bottom: 5px;'>"+d[0]+" "+d[2]+"</div>$"+d[1]+" Billion");
+                    tooltip.attr("data-date", d[0]);
+                })
+                .on('mouseout', (d) => {
+                    tooltip.style('opacity', 0);
+                })
+                .attr("class", "bar");
+
             //Draw the GDP Label:
             svg.append("text")
                 .attr("id", "title")
                 .attr("class", "headline")
-                .attr("x", w / 2) //positions it at the middle of the width
-                .attr("y", m.top) //positions it from the top by the margin top
+                .attr("x", w / 2)
+                .attr("y", m.top)
                 .attr("font-family", "sans-serif")
                 .attr("fill", "green")
                 .attr("text-anchor", "middle")
@@ -163,14 +176,14 @@ $(document).ready(() => {
             //Draw the GDP side Label:
             svg.append("text")
                 .attr("class", "headline1")
-                .attr("x", w / 3.6) //positions it at the middle of the width
-                .attr("y", h / 2.4) //positions it from the top by the margin top
+                .attr("x", w / 3.6)
+                .attr("y", h / 2.4)
                 .attr("transform", "translate(-180,530) rotate(-90)")
                 .attr("font-family", "sans-serif")
                 .attr("fill", "green")
                 .attr("text-anchor", "middle")
                 .text("Gross Domestic Product");  
-                
+            
         },
         error: (xhr, ajaxOptions, thrownError) => {
 
